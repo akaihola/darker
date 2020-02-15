ba112420760b4f159d0095c62c9b9aec77710afb
Antti Kaihola <antti.kaihola@eniram.fi>
2020-02-15 19:02:15 +0200

    Add some tests

---
+++ src/black_scm_diff/tests/test_parse.py
@@ -0,0 +1,34 @@
+import pytest
+from whatthepatch.patch import Change
+
+from black_scm_diff.__main__ import (get_edited_new_line_numbers,
+                                     choose_edited_lines)
+from black_scm_diff.tests.example_3_lines import CHANGE_SECOND_LINE
+
+
+def test_get_edited_line_numbers():
+    result = list(get_edited_new_line_numbers(CHANGE_SECOND_LINE))
+    assert result == [2]
+
+
+@pytest.mark.parametrize(
+    'edited_line_numbers, expect',
+    [([], [(1, 1, 'original first line', 1),
+           (3, 3, 'original third line', 1)]),
+     ([1], [(1, 1, 'original first line', 1),
+            (3, 3, 'original third line', 1)]),
+     ([2], [(1, 1, 'original first line', 1),
+            (2, None, 'original second line', 1),
+            (None, 2, 'changed second line', 1),
+            (3, 3, 'original third line', 1)]),
+     ([3], [(1, 1, 'original first line', 1),
+            (3, 3, 'original third line', 1)]),
+     ([1, 2], [(1, 1, 'original first line', 1),
+               (2, None, 'original second line', 1),
+               (None, 2, 'changed second line', 1),
+               (3, 3, 'original third line', 1)]),
+     ]
+)
+def test_choose_edited_lines(edited_line_numbers, expect):
+    result = list(choose_edited_lines(CHANGE_SECOND_LINE, edited_line_numbers))
+    assert [tuple(r) for r in result] == expect
---
+++ src/black_scm_diff/tests/test_whatthepatch.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+from whatthepatch import apply_diff
+from whatthepatch.patch import parse_patch, diffobj
+
+from black_scm_diff.tests.example_3_lines import ORIGINAL, CHANGE_SECOND_LINE
+
+
+def test_apply():
+    diff = next(parse_patch(CHANGE_SECOND_LINE))  # type: diffobj
+    old_content = ORIGINAL
+    new_content = apply_diff(diff, old_content)
+    assert new_content == ['original first line',
+                           'changed second line',
+                           'original third line']
+
+
+def test_apply_partial():
+    diff = next(parse_patch(CHANGE_SECOND_LINE))  # type: diffobj
+    del diff.changes[1:3]
+    old_content = ORIGINAL
+    new_content = apply_diff(diff, old_content)
+    assert new_content == ['original first line',
+                           'original second line',
+                           'original third line']
