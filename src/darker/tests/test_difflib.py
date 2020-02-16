7138bb8fd3ec9711c3e169739c0aa657191eeba3
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-02-16 13:54:39 +0200

    Run black on our own source code

--- src/darker/tests/test_difflib.py
+++ src/darker/tests/test_difflib.py
@@ -5,6 +5,8 @@ from darker.tests.example_3_lines import CHANGED, ORIGINAL
 
 def test_sequencematcher():
     s = SequenceMatcher(None, ORIGINAL.splitlines(), CHANGED.splitlines())
-    assert s.get_opcodes() == [('equal', 0, 1, 0, 1),
-                               ('replace', 1, 2, 1, 2),
-                               ('equal', 2, 3, 2, 3)]
+    assert s.get_opcodes() == [
+        ("equal", 0, 1, 0, 1),
+        ("replace", 1, 2, 1, 2),
+        ("equal", 2, 3, 2, 3),
+    ]
