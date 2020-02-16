ae652667dcc2aa7b965b2daa6fd77a7d974a726d
Antti Kaihola <antti.kaihola@eniram.fi>
2020-02-16 13:52:30 +0200

    More tests

---
+++ src/darker/tests/test_difflib.py
@@ -0,0 +1,10 @@
+from difflib import SequenceMatcher
+
+from darker.tests.example_3_lines import CHANGED, ORIGINAL
+
+
+def test_sequencematcher():
+    s = SequenceMatcher(None, ORIGINAL.splitlines(), CHANGED.splitlines())
+    assert s.get_opcodes() == [('equal', 0, 1, 0, 1),
+                               ('replace', 1, 2, 1, 2),
+                               ('equal', 2, 3, 2, 3)]
