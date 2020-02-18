c340eb307be63aa4428272de3d0b77d0bb4b2526
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-02-18 22:59:02 +0200

    Unit tests for `verify_ast_unchanged`

---
+++ src/darker/tests/test_verification.py
@@ -0,0 +1,21 @@
+import pytest
+
+from darker.verification import verify_ast_unchanged
+
+
+@pytest.mark.parametrize(
+    "src_lines, dst_content, expect",
+    [
+        (["if True: pass"], "if False: pass\n", AssertionError),
+        (["if True: pass"], "if True:\n    pass\n", None),
+    ],
+)
+def test_verify_ast_unchanged(src_lines, dst_content, expect):
+    black_chunks = [(1, ["black"], ["chunks"])]
+    edited_linenums = [1, 2]
+    try:
+        verify_ast_unchanged(src_lines, dst_content, black_chunks, edited_linenums)
+    except AssertionError:
+        assert expect is AssertionError
+    else:
+        assert expect is None
