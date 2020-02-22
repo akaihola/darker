2c85588c42d3de2edea821aed01a052c1f98241c
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-02-22 22:37:07 +0200

    Add tests for `--isort` option

---
+++ src/darker/tests/test_main.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from types import SimpleNamespace
+from unittest.mock import DEFAULT, Mock, patch
+
+import pytest
+
+import darker.__main__
+
+
+def test_isort_option_without_isort(without_isort, caplog):
+    with patch.object(darker.__main__, "SortImports", None), pytest.raises(SystemExit):
+
+        darker.__main__.main(["--isort", "/nowhere/dummy.py"])
+
+    assert (
+        "Please run `pip install 'darker[isort]'` to use the `--isort` option."
+        in caplog.text
+    )
+
+
+@pytest.fixture
+def run_isort(without_isort, caplog):
+    with patch.multiple(
+        darker.__main__, SortImports=DEFAULT, apply_black_on_edited_lines=DEFAULT
+    ):
+        darker.__main__.main(["--isort", "/nowhere/dummy.py"])
+        return SimpleNamespace(SortImports=darker.__main__.SortImports, caplog=caplog)
+
+
+def test_isort_option_with_isort(run_isort):
+    assert "Please run" not in run_isort.caplog.text
+
+
+def test_isort_option_with_isort_calls_sortimports(run_isort):
+    run_isort.SortImports.assert_called_once_with(
+        Path("/nowhere/dummy.py"),
+        force_grid_wrap=0,
+        include_trailing_comma=True,
+        line_length=88,
+        multi_line_output=3,
+        use_parentheses=True,
+    )
