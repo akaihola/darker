71fbbad89c617df57706742bd6ff3cb76cf2e07a
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-05-27 23:00:45 +0300

    Tests for git diff parse errors

--- src/darker/tests/test_git_diff.py
+++ src/darker/tests/test_git_diff.py
@@ -4,6 +4,7 @@ from textwrap import dedent
 import pytest
 
 from darker.git_diff import (
+    GitDiffParseError,
     GitDiffResult,
     get_edit_chunks,
     get_edit_chunks_for_one_file,
@@ -56,3 +57,106 @@ def test_get_edit_chunks_empty():
     gen = get_edit_chunks(GitDiffResult(b"", ["git", "diff"]))
     with pytest.raises(StopIteration):
         next(gen)
+
+
+@pytest.mark.parametrize(
+    "git_diff_lines",
+    [[], ["diff --git path_a path_b", "index ", "--- path_a", "+++ path_a"]],
+)
+def test_get_edit_chunks_empty_output(git_diff_lines):
+    git_diff_result = GitDiffResult(
+        "".join(f"{line}\n" for line in git_diff_lines).encode("ascii"),
+        ["git", "diff"],
+    )
+    result = list(get_edit_chunks(git_diff_result))
+    assert result == []
+
+
+@pytest.mark.parametrize(
+    "first_line",
+    ["diff --git ", "diff --git path_a", "diff --git path_a path_b path_c"],
+)
+def test_get_edit_chunks_cant_parse(first_line):
+    output = f"{first_line}\n"
+    git_diff_result = GitDiffResult(output.encode("ascii"), ["git", "diff"])
+    with pytest.raises(GitDiffParseError) as exc:
+        list(get_edit_chunks(git_diff_result))
+    assert str(exc.value) == f"Can't parse '{first_line}'"
+
+
+@pytest.mark.parametrize(
+    "git_diff_lines, expect",
+    [
+        (["first line doesn't have diff --git"], "diff --git ",),
+        (
+            ["diff --git path_a path_b", "second line doesn't have old mode"],
+            "old mode ",
+        ),
+        (
+            [
+                "diff --git path_a path_b",
+                "old mode ",
+                "third line doesn't have new mode",
+            ],
+            "new mode ",
+        ),
+        (
+            [
+                "diff --git path_a path_b",
+                "old mode ",
+                "new mode ",
+                "fourth line doesn't have index",
+            ],
+            "index ",
+        ),
+        (
+            [
+                "diff --git path_a path_b",
+                "index ",
+                "third line doesn't have --- path_a",
+            ],
+            "--- path_a",
+        ),
+        (
+            [
+                "diff --git path_a path_b",
+                "index ",
+                "--- path_a",
+                "fourth line doesn't have +++ path_a",
+            ],
+            "+++ path_a",
+        ),
+    ],
+)
+def test_get_edit_chunks_unexpected_line(git_diff_lines, expect):
+    git_diff_result = GitDiffResult(
+        "".join(f"{line}\n" for line in git_diff_lines).encode("ascii"),
+        ["git", "diff"],
+    )
+    with pytest.raises(GitDiffParseError) as exc:
+        list(get_edit_chunks(git_diff_result))
+    expect_exception_message = (
+        f"Expected an '{expect}' line, got '{git_diff_lines[-1]}' from 'git diff'"
+    )
+    assert str(exc.value) == expect_exception_message
+
+
+@pytest.mark.parametrize(
+    "git_diff_lines",
+    [
+        ["diff --git path_a path_b"],
+        ["diff --git path_a path_b", "old mode "],
+        ["diff --git path_a path_b", "old mode ", "new mode "],
+        ["diff --git path_a path_b", "old mode ", "new mode ", "index "],
+        ["diff --git path_a path_b", "index "],
+        ["diff --git path_a path_b", "index ", "--- path_a"],
+    ],
+)
+def test_get_edit_chunks_unexpected_end(git_diff_lines):
+    git_diff_result = GitDiffResult(
+        "".join(f"{line}\n" for line in git_diff_lines).encode("ascii"),
+        ["git", "diff"],
+    )
+    with pytest.raises(GitDiffParseError) as exc:
+        list(get_edit_chunks(git_diff_result))
+    assert str(exc.value) == "Unexpected end of output from 'git diff'"
