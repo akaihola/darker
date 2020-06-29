f572ba3c8f38c22c53b2a7980b7b5fc0f9d1c433
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-06-30 00:31:50 +0300

    Add tests for the `--diff` option
    Also adding tests for other options as well.

--- src/darker/tests/test_main.py
+++ src/darker/tests/test_main.py
@@ -7,7 +7,6 @@ import pytest
 
 import darker.__main__
 import darker.import_sorting
-from darker.tests.git_diff_example_output import CHANGE_SECOND_LINE
 
 
 def test_isort_option_without_isort(tmpdir, without_isort, caplog):
@@ -51,3 +50,86 @@ def test_isort_option_with_isort_calls_sortimports(run_isort):
         use_parentheses=True,
         quiet=True,
     )
+
+
+def test_format_edited_parts_empty():
+    with pytest.raises(ValueError):
+
+        darker.__main__.format_edited_parts([], False, {}, True)
+
+
+def test_format_edited_parts_isort_print_diff():
+    with pytest.raises(NotImplementedError):
+
+        darker.__main__.format_edited_parts([Path('test.py')], True, {}, True)
+
+
+A_PY = ['import sys', 'import os', "print( '42')", '']
+A_PY_BLACK = ['import sys', 'import os', '', 'print("42")', '']
+A_PY_BLACK_ISORT = ['import os', 'import sys', '', 'print("42")', '']
+
+A_PY_DIFF_BLACK = [
+    '--- /a.py',
+    '+++ /a.py',
+    '@@ -1,3 +1,4 @@',
+    '',
+    ' import sys',
+    ' import os',
+    "-print( '42')",
+    '+',
+    '+print("42")',
+    '',
+]
+
+A_PY_DIFF_BLACK_NO_STR_NORMALIZE = [
+    '--- /a.py',
+    '+++ /a.py',
+    '@@ -1,3 +1,4 @@',
+    '',
+    ' import sys',
+    ' import os',
+    "-print( '42')",
+    '+',
+    "+print('42')",
+    '',
+]
+
+
+@pytest.mark.parametrize(
+    'srcs, isort, black_args, print_diff, expect_stdout, expect_a_py',
+    [
+        (['a.py'], False, {}, True, A_PY_DIFF_BLACK, A_PY),
+        (['a.py'], True, {}, False, [''], A_PY_BLACK_ISORT),
+        (
+            ['a.py'],
+            False,
+            {'skip_string_normalization': True},
+            True,
+            A_PY_DIFF_BLACK_NO_STR_NORMALIZE,
+            A_PY,
+        ),
+        (['a.py'], False, {}, False, [''], A_PY_BLACK),
+    ],
+)
+def test_format_edited_parts(
+    git_repo,
+    monkeypatch,
+    capsys,
+    srcs,
+    isort,
+    black_args,
+    print_diff,
+    expect_stdout,
+    expect_a_py,
+):
+    monkeypatch.chdir(git_repo.root)
+    paths = git_repo.add({'a.py': '\n', 'b.py': '\n'}, commit='Initial commit')
+    paths['a.py'].write('\n'.join(A_PY))
+    paths['b.py'].write('print(42 )\n')
+    darker.__main__.format_edited_parts(
+        [Path(src) for src in srcs], isort, black_args, print_diff
+    )
+    stdout = capsys.readouterr().out.replace(str(git_repo.root), '')
+    assert stdout.split('\n') == expect_stdout
+    assert paths['a.py'].readlines(cr=False) == expect_a_py
+    assert paths['b.py'].readlines(cr=False) == ['print(42 )', '']
