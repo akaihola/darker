78f51ad40b1215d761fed763134ac2763ae73fce
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-08-08 18:10:37 +0300

    Functional test for the `--revision` option

--- src/darker/tests/test_main_revision.py
+++ src/darker/tests/test_main_revision.py
@@ -9,103 +9,103 @@ import pytest
 
 from darker.__main__ import main
 
+# The following test is a bit dense, so some explanation is due.
+#
+# A Git repository with 3 commits is created. Python files with one line of code
+# ("ORIGINAL=1") are added, modified or deleted in each commit. An abbreviated number is
+# used for each commit in the test code:
+# - 2: HEAD~2          add 4 files
+# - 1: HEAD~1 (HEAD^)  add 2 files, delete 1 file, keep 1 file, modify 2 files
+# - 0: HEAD~0 (HEAD)   delete 1 file, modify 1 file, keep 4 files
+#
+# In each test case, all of the 6 files are overwritten in the working tree with given
+# content.
+#
+# `darker --diff --revision <rev>` is called and the diff captured from stdout.
+#
+# Each case is gets as parameters:
+# - the `<rev>` argument – which Git revision to compare to
+# - `worktree_content` – the content to write to all the 6 known Python files
+# - `expect` – a list of files expected to appear in `darker --diff`
+#
+# The Python file names tell the history of the file with a list of actions:
+# +2 - added in HEAD~2
+# +1 - added in HEAD^
+# -1 - deleted in HEAD^
+# M1 - modified in HEAD^
+# -0 - deleted in HEAD
+# M0 - modified in HEAD
+#
+# So e.g. the test case `HEAD~2`, `ORIGINAL=1` tells us that when `ORIGINAL=1` is
+# written to all Python files and `darker --diff --revision HEAD~2` is called, the diff
+# indicates reformatting in
+# - +1.py – the file which was added in HEAD^
+# - +1M0.py – the file added in HEAD^ and overwritten in HEAD (with `MODIFIED=1`)
+# All other files are missing from the diff since their content was the same as now
+# (`ORIGINAL=1`) at HEAD~2.
+
 
 @pytest.mark.parametrize(
-    'revision, expect',
+    "revision, worktree_content, expect",
     [
-        (
-            "",
-            [
-                "appear",
-                "appear_and_modify",
-                "delete",
-                "modify",
-                "modify_and_delete",
-                "never_change",
-            ],
-        ),
-        (
-            "HEAD",
-            [
-                "appear",
-                "appear_and_modify",
-                "delete",
-                "modify",
-                "modify_and_delete",
-                "never_change",
-            ],
-        ),
-        (
-            "HEAD^",
-            [
-                "appear",
-                "appear_and_modify",
-                "delete",
-                "modify",
-                "modify_and_delete",
-                "never_change",
-            ],
-        ),
-        (
-            "HEAD~2",
-            [
-                "appear",
-                "appear_and_modify",
-                "delete",
-                "modify",
-                "modify_and_delete",
-                "never_change",
-            ],
-        ),
-        ("HEAD~3", SystemExit),
+        ("", "USERMOD=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("", "ORIGINAL=1\n", ["+1M0", "+2-1", "+2M1-0", "+2M1"]),
+        ("", "MODIFIED=1\n", ["+1", "+2-1", "+2", "+2M1-0"]),
+        ("HEAD", "USERMOD=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("HEAD", "ORIGINAL=1\n", ["+1M0", "+2-1", "+2M1-0", "+2M1"],),
+        ("HEAD", "MODIFIED=1\n", ["+1", "+2-1", "+2", "+2M1-0"]),
+        ("HEAD^", "USERMOD=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("HEAD^", "USERMOD=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("HEAD^", "ORIGINAL=1\n", ["+2-1", "+2M1-0", "+2M1"]),
+        ("HEAD^", "MODIFIED=1\n", ["+1", "+1M0", "+2-1", "+2"]),
+        ("HEAD~2", "USERMOD=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("HEAD~2", "ORIGINAL=1\n", ["+1", "+1M0"]),
+        ("HEAD~2", "MODIFIED=1\n", ["+1", "+1M0", "+2-1", "+2", "+2M1-0", "+2M1"]),
+        ("HEAD~3", "USERMOD=1\n", SystemExit),
     ],
 )
-def test_revision(git_repo, monkeypatch, capsys, revision, expect):
+def test_revision(git_repo, monkeypatch, capsys, revision, worktree_content, expect):
     monkeypatch.chdir(git_repo.root)
+    # 2: HEAD~2:
     paths = git_repo.add(
         {
-            'never_change.py': 'ORIGINAL = 1\n',
-            'modify.py': 'ORIGINAL = 1\n',
-            'delete.py': 'ORIGINAL = 1\n',
-            'modify_and_delete.py': 'ORIGINAL = 1\n',
+            "+2.py": "ORIGINAL=1\n",
+            "+2M1.py": "ORIGINAL=1\n",
+            "+2-1.py": "ORIGINAL=1\n",
+            "+2M1-0.py": "ORIGINAL=1\n",
         },
-        commit='First commit',
+        commit="First commit",
     )
+    # 1: HEAD~1 i.e. HEAD^
     paths.update(
         git_repo.add(
             {
-                'modify.py': 'MODIFIED = 1\n',
-                'appear.py': 'ADDED = 1\n',
-                'appear_and_modify.py': 'ORIGINAL = 1\n',
-                'delete.py': None,
-                'modify_and_delete.py': 'MODIFIED = 1\n',
+                "+2M1.py": "MODIFIED=1\n",
+                "+1.py": "ORIGINAL=1\n",
+                "+1M0.py": "ORIGINAL=1\n",
+                "+2-1.py": None,
+                "+2M1-0.py": "MODIFIED=1\n",
             },
-            commit='Second commit',
+            commit="Second commit",
         )
     )
+    # 0: HEAD~0 i.e. HEAD:
     git_repo.add(
-        {'appear_and_modify.py': 'MODIFIED = 1\n', 'modify_and_delete.py': None},
-        commit='Third commit',
+        {"+1M0.py": "MODIFIED=1\n", "+2M1-0.py": None}, commit="Third commit",
     )
+    # Working tree:
     for path in paths.values():
-        path.write('USER_MODIFICATION=1\n')
-    arguments = ["--diff", "--revision", revision, '.']
+        path.write(worktree_content)
+    arguments = ["--diff", "--revision", revision, "."]
     should_raise = expect is SystemExit
 
     with pytest.raises(SystemExit) if should_raise else nullcontext():
         main(arguments)
 
     if not should_raise:
-        expect_diff_output = [
-            line
-            for name in expect
-            for line in [
-                f'--- {name}.py',
-                f'+++ {name}.py',
-                '@@ -1 +1 @@',
-                '',
-                '-USER_MODIFICATION=1',
-                '+USER_MODIFICATION = 1',
-            ]
+        modified_files = [
+            line[4:-3]
+            for line in capsys.readouterr().out.splitlines()
+            if line.startswith("+++ ")
         ]
-        assert capsys.readouterr().out.splitlines() == expect_diff_output
+        assert modified_files == expect
