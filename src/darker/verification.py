0b17a16136633efc22bc067536f0c5f1d82e99a6
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2021-05-01 22:35:11 +0300

    Binary search to minimize number of context lines
    If AST differs after reformatting when diffing and chunking with zero
    context lines, search for the lowest successful number of context
    lines using a binary search instead of by incrementing one by
    one. This vastly improves performance in such problematic cases.

--- src/darker/verification.py
+++ src/darker/verification.py
@@ -11,6 +11,50 @@ class NotEquivalentError(Exception):
     pass
 
 
+class BinarySearch:
+    """Effectively search the first index for which a condition is ``True``
+
+    Example usage to find first number between 0 and 100 whose square is larger than
+    1000::
+
+        s = BinarySearch(0, 100)
+        while not s.found:
+            s.respond(s.get_next() ** 2 > 1000)
+        answer = s.result
+
+    """
+
+    def __init__(self, low: int, high: int):
+        self.low = self.mid = low
+        self.high = high
+
+    def get_next(self) -> int:
+        """Get the next integer index to try"""
+        return self.mid
+
+    def respond(self, value: bool) -> None:
+        """Provide a ``False`` or ``True`` answer for the current integer index"""
+        if value:
+            self.high = self.mid
+        else:
+            self.low = self.mid + 1
+        self.mid = (self.low + self.high) // 2
+
+    @property
+    def found(self) -> bool:
+        """Return ``True`` if the lowest ``True`` response index has been found"""
+        return self.low >= self.high
+
+    @property
+    def result(self) -> int:
+        """Return the lowest index with a ``True`` response"""
+        if not self.found:
+            raise RuntimeError(
+                "Trying to get binary search result before search has been exhausted"
+            )
+        return self.high
+
+
 def verify_ast_unchanged(
     edited_to_file: TextDocument,
     reformatted: TextDocument,
