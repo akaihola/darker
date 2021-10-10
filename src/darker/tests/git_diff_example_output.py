86bcf51ae35b999721b3151eee60dc6571e595fd
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2021-10-10 21:48:16 +0300

    Additional tests for `difflib`
    These make sure `difflib` behaves as we expect.

--- src/darker/tests/git_diff_example_output.py
+++ src/darker/tests/git_diff_example_output.py
@@ -15,3 +15,64 @@ CHANGED = dedent(
     original third line
 '''
 )
+
+BLANK_SEP_ORIGINAL = dedent(
+    """\
+    original first line
+
+    original third line
+
+    original fifth line
+    """
+)
+
+BLANK_SEP_CHANGED = dedent(
+    """\
+    changed first line
+
+    original third line
+
+    changed fifth line
+    """
+)
+
+BLANK_SEP_SANDWICH_ORIGINAL = dedent(
+    """\
+    original first line
+
+    original third line
+
+    original fifth line
+    """
+)
+
+BLANK_SEP_SANDWICH_CHANGED = dedent(
+    """\
+    changed first line
+
+    original third line
+
+    changed fifth line
+    """
+)
+
+
+BLANK_SEP_ORIGINAL = dedent(
+    """\
+    original first line
+
+    original third line
+
+    original fifth line
+    """
+)
+
+BLANK_SEP_CHANGED = dedent(
+    """\
+    changed first line
+
+    changed third line
+
+    changed fifth line
+    """
+)
