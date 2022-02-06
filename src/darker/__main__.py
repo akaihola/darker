9bca60d47858f43ee4a17aea1f1986f50c1f0ae9
Sourcery AI <>
2022-02-06 18:12:57 +0000

    'Refactored by Sourcery'

--- src/darker/__main__.py
+++ src/darker/__main__.py
@@ -360,8 +360,7 @@ def main(argv: List[str] = None) -> int:
                 " Either --diff or --check must be used.",
             )
 
-    missing = get_missing_at_revision(paths, revrange.rev2, root)
-    if missing:
+    if missing := get_missing_at_revision(paths, revrange.rev2, root):
         missing_reprs = " ".join(repr(str(path)) for path in missing)
         rev2_repr = "the working tree" if revrange.rev2 == WORKTREE else revrange.rev2
         raise ArgumentError(
