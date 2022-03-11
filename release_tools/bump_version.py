6040722f91b4f723bd30ab8fd763054894abf085
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-03-11 22:52:56 +0200

    Increment milestone in README in `bump_version.py`

--- release_tools/bump_version.py
+++ release_tools/bump_version.py
@@ -15,10 +15,11 @@ import re
 import sys
 from datetime import date
 from pathlib import Path
-from typing import Tuple
+from typing import Dict, Tuple
 
 import click
-from packaging.version import Version, parse
+import requests
+from packaging.version import Version
 
 if sys.version_info >= (3, 8):
     from typing import TypedDict
@@ -29,27 +30,100 @@ else:
 VERSION_PY_PATH = "src/darker/version.py"
 
 PATTERNS = {
-    VERSION_PY_PATH: {r"^__version__ *= *\"{version}\""},
+    VERSION_PY_PATH: {r"^__version__ *= *\"{old_version->new_version}\""},
     "action.yml": {
-        r'^    description: \'Python Version specifier \(PEP440\) - e\.g\. "{version}"',
-        r'^    default: "{version}"',
-        r"^      uses: akaihola/darker/.github/actions/commit-range@{version}",
+        (
+            r"^    description: \'Python Version specifier \(PEP440\) - e\.g\."
+            r' "{old_version->new_version}"'
+        ),
+        r'^    default: "{old_version->new_version}"',
+        (
+            r"^      uses: akaihola/darker/.github/actions/commit-range"
+            r"@{old_version->new_version}"
+        ),
     },
     "README.rst": {
-        r"^           rev: {version}",
-        r"^       rev: {version}",
-        r"^         - uses: akaihola/darker@{version}",
-        r'^             version: "{version}"',
+        r"^           rev: {old_version->new_version}",
+        r"^       rev: {old_version->new_version}",
+        r"^         - uses: akaihola/darker@{old_version->new_version}",
+        r'^             version: "{old_version->new_version}"',
+        r"label=release%20{new_version->next_version}",
+        (
+            r"^\.\. \|next-milestone\| image::"
+            r" https://img\.shields\.io/github/milestones/progress/akaihola/darker/"
+            r"{new_milestone->next_milestone}"
+        ),
+        (
+            r"^\.\. _next-milestone:"
+            r" https://github\.com/akaihola/darker/milestone/"
+            r"{new_milestone->next_milestone}"
+        ),
     },
     ".github/ISSUE_TEMPLATE/bug_report.md": {
-        r"^ - Darker version \[e\.g\. {version}\]"
+        r"^ - Darker version \[e\.g\. {old_version->new_version}\]"
     },
 }
 
-SEARCH_CURRENT_VERSION = re.compile(
-    next(iter(PATTERNS[VERSION_PY_PATH])).format(version=r"([\d\.a-z]+)"),
+CURRENT_VERSION_RE = re.compile(
+    next(iter(PATTERNS[VERSION_PY_PATH])).format(
+        **{"old_version->new_version": r"([\d\.a-z]+)"}
+    ),
     flags=re.MULTILINE,
-).search
+)
+
+
+CAPTURE_RE = re.compile(r"\{(\w+)->(\w+)\}")
+
+
+def get_milestone_numbers() -> Dict[Version, str]:
+    """Fetch milestone names and numbers from the GitHub API
+
+    :return: Milestone names as version numbers, and corresponding milestone numbers
+    :raises TypeError: Raised on unexpected JSON response
+
+    """
+    milestones = requests.get(
+        "https://api.github.com/repos/akaihola/darker/milestones"
+    ).json()
+    if not isinstance(milestones, list):
+        raise TypeError(f"Expected a JSON list from GitHub API, got {milestones}")
+    return {Version(m["title"]): str(m["number"]) for m in milestones}
+
+
+def get_next_milestone_version(
+    version: Version, milestone_numbers: Dict[Version, str]
+) -> Version:
+    """Get the next larger version number found among milestone names
+
+    :param version: The version number to search a larger one for
+    :param milestone_numbers: Milestone names and numbers from the GitHub API
+    :return: The next larger version number found
+    :raises RuntimeError: Raised if no larger version number could be found
+
+    """
+    for milestone_version in sorted(milestone_numbers):
+        if milestone_version > version:
+            return milestone_version
+    raise RuntimeError(f"No milestone exists for a version later than {version}")
+
+
+class NoMatch(Exception):
+    """Raised if pattern couldn't be found in the content"""
+
+
+def replace_span(span: Tuple[int, int], replacement: str, content: str) -> str:
+    """Replace given span in a string with the desired replacement string
+
+    :param span: The span to replace
+    :param replacement: The string to use as the replacement
+    :param content: The content to replace the span in
+    :return: The result after the replacement
+
+    """
+    start, end = span
+    before = content[:start]
+    after = content[end:]
+    return f"{before}{replacement}{after}"
 
 
 @click.command()
@@ -58,57 +132,132 @@ SEARCH_CURRENT_VERSION = re.compile(
 @click.option("-m", "--minor", "increment_minor", is_flag=True, default=False)
 def bump_version(dry_run: bool, increment_major: bool, increment_minor: bool) -> None:
     """Bump the version number"""
-    current_version = get_current_version()
-    current_version_pattern = re.escape(str(current_version))
-    next_version = get_next_version(current_version, increment_major, increment_minor)
+    (patterns, replacements, new_version) = get_replacements(
+        increment_major, increment_minor
+    )
     for path_str, pattern_templates in PATTERNS.items():
         path = Path(path_str)
         content = path.read_text(encoding="utf-8")
         for pattern_template in pattern_templates:
-            (version_number_start, version_number_end) = get_version_number_span(
-                pattern_template, current_version_pattern, content
+            # example: pattern_template == r"darker/{new_milestone->next_milestone}"
+            template_match = CAPTURE_RE.search(pattern_template)
+            if not template_match:
+                raise NoMatch("Can't find `{CAPTURE_RE}` in `{pattern_template}`")
+            current_pattern_name, replacement_name = template_match.groups()
+            # example: template_match.groups() == ("new_milestone", "next_milestone")
+            current_pattern = patterns[current_pattern_name]  # type: ignore[misc]
+            # example: current_pattern == "14"
+            replacement = replacements[replacement_name]  # type: ignore[misc]
+            # example: replacement == "15"
+            pattern = replace_span(
+                template_match.span(), f"({current_pattern})", pattern_template
             )
-            before = content[:version_number_start]
-            after = content[version_number_end:]
-            content = f"{before}{next_version}{after}"
+            # example: pattern = r"darker/(14)"
+            match = re.search(pattern, content, flags=re.MULTILINE)
+            if not match:
+                raise NoMatch(f"Can't find `{pattern}` in `{path_str}`")
+            content = replace_span(match.span(1), replacement, content)
         if dry_run:
             print(f"\n######## {path_str} ########\n")
             print(content)
-    patch_changelog(next_version, dry_run)
+        else:
+            path.write_text(content, encoding="utf-8")
+    patch_changelog(new_version, dry_run)
+
+
+class PatternDict(TypedDict):
+    """Patterns for old and new version and the milestone number for the new version"""
+
+    old_version: str
+    new_version: str
+    new_milestone: str
+
+
+class ReplacementDict(TypedDict):
+    """Replacement strings of new and next version and milestone num for next version"""
+
+    new_version: str
+    next_version: str
+    next_milestone: str
+
+
+def get_replacements(
+    increment_major: bool, increment_minor: bool
+) -> Tuple[PatternDict, ReplacementDict, Version]:
+    """Return search patterns and replacements for version numbers and milestones
+
+    Gets the current version from `version.py` and the milestone numbers from the GitHub
+    API. Based on these, builds the search patterns for the old and new version numbers
+    and the milestone number of the new version, as well as replacement strings for the
+    new and next version numbers and the milestone number of the next version.
+
+    :param increment_major: `True` to increment the major version number
+    :param increment_minor: `True` to increment the minor version number
+    :return: Patterns, replacements and the new version number
+
+    """
+    old_version = get_current_version()
+    new_version = get_next_version(old_version, increment_major, increment_minor)
+    milestone_numbers = get_milestone_numbers()
+    next_version = get_next_milestone_version(new_version, milestone_numbers)
+    patterns: PatternDict = {
+        "old_version": re.escape(str(old_version)),
+        "new_version": re.escape(str(new_version)),
+        "new_milestone": milestone_numbers[new_version],
+    }
+    replacements: ReplacementDict = {
+        "new_version": str(new_version),
+        "next_version": str(next_version),
+        "next_milestone": milestone_numbers[next_version],
+    }
+    return patterns, replacements, new_version
 
 
 def get_current_version() -> Version:
-    version_py = Path("src/darker/version.py").read_text(encoding="utf-8")
-    current_version = SEARCH_CURRENT_VERSION(version_py).group(1)
-    return parse(current_version)
+    """Find the current version number from `version.py`
+
+    :return: The current version number
+    :raises NoMatch: Raised if `version.py` doesn't match the expected format
+
+    """
+    version_py = Path(VERSION_PY_PATH).read_text(encoding="utf-8")
+    match = CURRENT_VERSION_RE.search(version_py)
+    if not match:
+        raise NoMatch("Can't find `{SEARCH_CURRENT_VERSION}` in `{VERSION_PY_PATH}`")
+    current_version = match.group(1)
+    return Version(current_version)
 
 
 def get_next_version(
     current_version: Version, increment_major: bool, increment_minor: bool
 ) -> Version:
+    """Return the next version number by incrementing elements as specified
+
+    :param current_version: The version number to increment
+    :param increment_major: `True` to increment the major version number
+    :param increment_minor: `True` to increment the minor version number
+    :return: The new version number
+
+    """
     major, minor, micro = current_version.release
     if increment_major:
-        return f"{major + 1}.0.0"
+        return Version(f"{major + 1}.0.0")
     if increment_minor:
-        return f"{major}.{minor + 1}.0"
+        return Version(f"{major}.{minor + 1}.0")
     if current_version.is_devrelease or current_version.is_prerelease:
-        return str(current_version)
-    return f"{major}.{minor}.{micro + 1}"
+        return current_version
+    return Version(f"{major}.{minor}.{micro + 1}")
 
 
-def get_version_number_span(
-    pattern_template: str, current_version_pattern: str, content: str
-) -> Tuple[int, int]:
-    pattern = pattern_template.format(version=f"({current_version_pattern})")
-    match = re.search(pattern, content, flags=re.MULTILINE)
-    if not match:
-        raise RuntimeError(f"Can't find r'{pattern}' in `{path_str}`")
-    _, version_number_span = match.regs
-    return version_number_span
+def patch_changelog(next_version: Version, dry_run: bool) -> None:
+    """Insert the new version and create a new unreleased section in the change log
 
+    :param next_version: The next version after the new version
+    :param dry_run: `True` to just print the result
 
-def patch_changelog(next_version: str, dry_run: bool) -> None:
-    content = Path("CHANGES.rst").read_text(encoding="utf-8")
+    """
+    path = Path("CHANGES.rst")
+    content = path.read_text(encoding="utf-8")
     before_unreleased = "These features will be included in the next release:\n\n"
     insert_point = content.index(before_unreleased) + len(before_unreleased)
     before = content[:insert_point]
@@ -127,7 +276,9 @@ def patch_changelog(next_version: str, dry_run: bool) -> None:
     if dry_run:
         print("######## CHANGES.rst ########")
         print(new_content[:200])
+    else:
+        path.write_text(new_content, encoding="utf-8")
 
 
 if __name__ == "__main__":
-    bump_version()
+    bump_version()  # pylint: disable=no-value-for-parameter
