0db6be4badac1d5b3319c1d2ab6f9c29ec1a7f77
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-03-11 22:59:45 +0200

    Improvements and fixes to `bump_version.py`
    - Document, refactor and reorder the code a bit
    - Fix module docstring
    - Python <3.9 compatibility
    
    Importing failed when running Pytest. Instead of carefully preventing
    collecting this module for tests in Python <3.9, let's just simply make
    it compatible.

--- release_tools/bump_version.py
+++ release_tools/bump_version.py
@@ -7,7 +7,14 @@ without modifications. Hence this script.
 
 Usage::
 
-    python release_tools/bump_version.py {major|minor|patch}
+    python release_tools/bump_version.py {--major|--minor} [--dry-run]
+
+Increments the patch version by default unless `--major` or `--minor` is specified.
+With `--dry-run` will print out modified files on the terminal or crash with an
+exception and a non-zero return value.
+
+`.github/workflows/test-bump-version.yml` runs this with `--dry-run` to ensure all
+regular expressions match content of the files to modify.
 
 """
 
@@ -15,7 +22,7 @@ import re
 import sys
 from datetime import date
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, Match, Tuple
 
 import click
 import requests
@@ -28,6 +35,27 @@ else:
 
 
 VERSION_PY_PATH = "src/darker/version.py"
+
+
+# Below are the regular expression patterns for finding and replacing version and
+# milestone numbers in files. Keys are file paths relative to the repository root.
+# Values are sets of regular expression pattern strings which contain a magic
+# `{OLD->NEW}` expression. For matching text, that expression will be turned into a
+# regular expression string which matches the expected version or milestone string in
+# the current content of a file. For replacing those matches with updated information,
+# `NEW` specifies which kind of a version or milestone number should be used as the
+# replacement.
+#
+# For example, if the current version ("old_version") was `1.0.1` and bumping the minor
+# version was requested, the entry
+#
+#   `"README.rst": {r"next version: {old_version->new_version}"}`
+#
+# would find
+#
+#   `r"next version: (1\.0\.1)"`
+#
+# in `README.rst` and replace the text matched by the capture group with "1.1".
 
 PATTERNS = {
     VERSION_PY_PATH: {r"^__version__ *= *\"{old_version->new_version}\""},
@@ -64,67 +92,6 @@ PATTERNS = {
     },
 }
 
-CURRENT_VERSION_RE = re.compile(
-    next(iter(PATTERNS[VERSION_PY_PATH])).format(
-        **{"old_version->new_version": r"([\d\.a-z]+)"}
-    ),
-    flags=re.MULTILINE,
-)
-
-
-CAPTURE_RE = re.compile(r"\{(\w+)->(\w+)\}")
-
-
-def get_milestone_numbers() -> Dict[Version, str]:
-    """Fetch milestone names and numbers from the GitHub API
-
-    :return: Milestone names as version numbers, and corresponding milestone numbers
-    :raises TypeError: Raised on unexpected JSON response
-
-    """
-    milestones = requests.get(
-        "https://api.github.com/repos/akaihola/darker/milestones"
-    ).json()
-    if not isinstance(milestones, list):
-        raise TypeError(f"Expected a JSON list from GitHub API, got {milestones}")
-    return {Version(m["title"]): str(m["number"]) for m in milestones}
-
-
-def get_next_milestone_version(
-    version: Version, milestone_numbers: Dict[Version, str]
-) -> Version:
-    """Get the next larger version number found among milestone names
-
-    :param version: The version number to search a larger one for
-    :param milestone_numbers: Milestone names and numbers from the GitHub API
-    :return: The next larger version number found
-    :raises RuntimeError: Raised if no larger version number could be found
-
-    """
-    for milestone_version in sorted(milestone_numbers):
-        if milestone_version > version:
-            return milestone_version
-    raise RuntimeError(f"No milestone exists for a version later than {version}")
-
-
-class NoMatch(Exception):
-    """Raised if pattern couldn't be found in the content"""
-
-
-def replace_span(span: Tuple[int, int], replacement: str, content: str) -> str:
-    """Replace given span in a string with the desired replacement string
-
-    :param span: The span to replace
-    :param replacement: The string to use as the replacement
-    :param content: The content to replace the span in
-    :return: The result after the replacement
-
-    """
-    start, end = span
-    before = content[:start]
-    after = content[end:]
-    return f"{before}{replacement}{after}"
-
 
 @click.command()
 @click.option("-n", "--dry-run", is_flag=True, default=False)
@@ -143,20 +110,15 @@ def bump_version(dry_run: bool, increment_major: bool, increment_minor: bool) ->
             template_match = CAPTURE_RE.search(pattern_template)
             if not template_match:
                 raise NoMatch("Can't find `{CAPTURE_RE}` in `{pattern_template}`")
-            current_pattern_name, replacement_name = template_match.groups()
-            # example: template_match.groups() == ("new_milestone", "next_milestone")
-            current_pattern = patterns[current_pattern_name]  # type: ignore[misc]
-            # example: current_pattern == "14"
-            replacement = replacements[replacement_name]  # type: ignore[misc]
-            # example: replacement == "15"
+            current_pattern, replacement = lookup_patterns(
+                template_match, patterns, replacements
+            )
+            # example: current_pattern == "14", replacement == "15"
             pattern = replace_span(
                 template_match.span(), f"({current_pattern})", pattern_template
             )
             # example: pattern = r"darker/(14)"
-            match = re.search(pattern, content, flags=re.MULTILINE)
-            if not match:
-                raise NoMatch(f"Can't find `{pattern}` in `{path_str}`")
-            content = replace_span(match.span(1), replacement, content)
+            content = replace_group_1(pattern, replacement, content, path=path_str)
         if dry_run:
             print(f"\n######## {path_str} ########\n")
             print(content)
@@ -166,7 +128,17 @@ def bump_version(dry_run: bool, increment_major: bool, increment_minor: bool) ->
 
 
 class PatternDict(TypedDict):
-    """Patterns for old and new version and the milestone number for the new version"""
+    r"""Patterns for old and new version and the milestone number for the new version
+
+    Example:
+
+    >>> patterns: PatternDict = {
+    ...     "old_version": r"1\.0",
+    ...     "new_version": r"1\.1",
+    ...     "new_milestone": r"22",
+    ... }
+
+    """
 
     old_version: str
     new_version: str
@@ -174,11 +146,29 @@ class PatternDict(TypedDict):
 
 
 class ReplacementDict(TypedDict):
-    """Replacement strings of new and next version and milestone num for next version"""
+    """Replacement strings of new and next version and milestone num for next version
+
+    Example:
+
+    >>> replacement: ReplacementDict = {
+    ...     "new_version": "1.1",
+    ...     "next_version": "2.0",
+    ...     "next_milestone": "23",
+    ... }
+
+    """
 
     new_version: str
     next_version: str
     next_milestone: str
+
+
+if sys.version_info >= (3, 9):
+    PATTERN_NAMES = PatternDict.__required_keys__  # type: ignore[attr-defined]  # pylint: disable=no-member  # noqa
+    REPLACEMENT_NAMES = ReplacementDict.__required_keys__  # type: ignore[attr-defined]  # pylint: disable=no-member  # noqa
+else:
+    PATTERN_NAMES = PatternDict.__annotations__  # pylint: disable=no-member
+    REPLACEMENT_NAMES = ReplacementDict.__annotations__  # pylint: disable=no-member
 
 
 def get_replacements(
@@ -228,6 +218,18 @@ def get_current_version() -> Version:
     return Version(current_version)
 
 
+CURRENT_VERSION_RE = re.compile(
+    next(iter(PATTERNS[VERSION_PY_PATH])).format(
+        **{"old_version->new_version": r"([\d\.a-z]+)"}
+    ),
+    flags=re.MULTILINE,
+)
+
+
+class NoMatch(Exception):
+    """Raised if pattern couldn't be found in the content"""
+
+
 def get_next_version(
     current_version: Version, increment_major: bool, increment_minor: bool
 ) -> Version:
@@ -247,6 +249,128 @@ def get_next_version(
     if current_version.is_devrelease or current_version.is_prerelease:
         return current_version
     return Version(f"{major}.{minor}.{micro + 1}")
+
+
+def get_milestone_numbers() -> Dict[Version, str]:
+    """Fetch milestone names and numbers from the GitHub API
+
+    :return: Milestone names as version numbers, and corresponding milestone numbers
+    :raises TypeError: Raised on unexpected JSON response
+
+    """
+    milestones = requests.get(
+        "https://api.github.com/repos/akaihola/darker/milestones"
+    ).json()
+    if not isinstance(milestones, list):
+        raise TypeError(f"Expected a JSON list from GitHub API, got {milestones}")
+    return {Version(m["title"]): str(m["number"]) for m in milestones}
+
+
+CAPTURE_RE = re.compile(r"\{(\w+)->(\w+)\}")
+
+
+def lookup_patterns(
+    template_match: Match[str], patterns: PatternDict, replacements: ReplacementDict
+) -> Tuple[str, str]:
+    r"""Look up the search pattern and replacement for the given search->replace names
+
+    `patterns` must contain regular expressions for finding the old version, the new
+    version, and the milestone number corresponding to the new version.
+
+    `replacements` must contain strings for the new version number, the next version
+    number after that, and the milestone number corresponding to the next version
+    number.
+
+    This function accepts a regular expression match object for a `{OLD->NEW}` string,
+    finds the pattern corresponding to the `OLD` string from `patterns`, finds the
+    replacement corresponding to the `NEW` string form `replacements`, and returns them
+    both.
+
+    Example:
+
+    >>> patterns = {"new_version": r"1\.1"}
+    >>> replacements = {"next_version": "2.0"}
+    >>> template_match = re.match(r"(.*)->(.*)", "new_version->next_version")
+    >>> lookup_patterns(template_match, patterns, replacements)
+    ('1\\.1', '2.0')
+
+    :param template_match: The match object with pattern name and replacement name as
+                           capture groups
+    :param patterns: The regular expression patterns corresponding to pattern names
+    :param replacements: The replacement strings corresponding to replacement names
+    :raises RuntimeError: Raised if pattern or replacement names are unknown
+    :return: The matching regular expression pattern and replacement string
+
+    """
+    current_pattern_name, replacement_name = template_match.groups()
+    # example: template_match.groups() == ("new_milestone", "next_milestone")
+    if current_pattern_name not in PATTERN_NAMES:
+        raise RuntimeError(
+            f"Pattern name {current_pattern_name!r} for a current value is"
+            f" unknown. Valid pattern names: {PATTERN_NAMES}"
+        )
+    current_pattern = patterns[current_pattern_name]  # type: ignore[literal-required]
+    # example: current_pattern == "14"
+    if replacement_name not in REPLACEMENT_NAMES:
+        raise RuntimeError(
+            f"Replacement name {replacement_name!r} is unknown. Valid"
+            f" replacement names: {REPLACEMENT_NAMES}"
+        )
+    replacement = replacements[replacement_name]  # type: ignore[literal-required]
+    # example: replacement == "15"
+    return current_pattern, replacement
+
+
+def get_next_milestone_version(
+    version: Version, milestone_numbers: Dict[Version, str]
+) -> Version:
+    """Get the next larger version number found among milestone names
+
+    :param version: The version number to search a larger one for
+    :param milestone_numbers: Milestone names and numbers from the GitHub API
+    :return: The next larger version number found
+    :raises RuntimeError: Raised if no larger version number could be found
+
+    """
+    for milestone_version in sorted(milestone_numbers):
+        if milestone_version > version:
+            return milestone_version
+    raise RuntimeError(f"No milestone exists for a version later than {version}")
+
+
+def replace_span(span: Tuple[int, int], replacement: str, content: str) -> str:
+    """Replace given span in a string with the desired replacement string
+
+    :param span: The span to replace
+    :param replacement: The string to use as the replacement
+    :param content: The content to replace the span in
+    :return: The result after the replacement
+
+    """
+    start, end = span
+    before = content[:start]
+    after = content[end:]
+    return f"{before}{replacement}{after}"
+
+
+def replace_group_1(pattern: str, replacement: str, content: str, path: str) -> str:
+    """Replace the first capture group of a regex pattern with the given string
+
+    Raises an exception if the regular expression doesn't match.
+
+    :param pattern: The regular expression pattern with at least one capture group
+    :param replacement: The string to replace the capture group with
+    :param content: The content to search and do the replacement in
+    :param path: The originating file path for the content. Only used in the exception
+                 message if the regular expression doesn't find any matches.
+    :raises NoMatch: Raised if the regular expression doesn't find any matches
+    :return: The resulting content after the replacement
+
+    """
+    match = re.search(pattern, content, flags=re.MULTILINE)
+    if not match:
+        raise NoMatch(f"Can't find `{pattern}` in `{path}`")
+    return replace_span(match.span(1), replacement, content)
 
 
 def patch_changelog(next_version: Version, dry_run: bool) -> None:
