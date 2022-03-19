e7fe8a5e73afeb69beeb7b067a2e4c85f0c25a34
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-03-19 23:19:41 +0200

    Comma-separated `lint:` option in gh action
    - add tests for GH Action script
    - whitelist linters to protect against PyPI name-squatting

--- action/main.py
+++ action/main.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 from subprocess import PIPE, STDOUT, run  # nosec
 
+LINTER_WHITELIST = {"flake8", "pylint", "mypy"}
 ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
 ENV_PATH = ACTION_PATH / ".darker-env"
 ENV_BIN = ENV_PATH / ("Scripts" if sys.platform == "win32" else "bin")
@@ -20,10 +21,16 @@ req = ["darker[isort]"]
 if VERSION:
     req[0] += f"=={VERSION}"
 linter_options = []
-for linter in ["flake8", "mypy", "pylint"]:
-    if os.getenv(f"INPUT_{linter.upper()}", default=""):
-        req.append(linter)
-        linter_options.append(f"--{linter}")
+for linter_ in os.getenv("INPUT_LINT", default="").split(","):
+    linter = linter_.strip()
+    if not linter:
+        continue
+    if linter not in LINTER_WHITELIST:
+        raise RuntimeError(
+            f"{linter!r} is not supported as a linter by the GitHub Action"
+        )
+    req.append(linter)
+    linter_options.extend(["--lint", linter])
 
 pip_proc = run(  # nosec
     [str(ENV_BIN / "python"), "-m", "pip", "install"] + req,
@@ -33,8 +40,8 @@ pip_proc = run(  # nosec
     encoding="utf-8",
 )
 if pip_proc.returncode:
-    print(pip_proc.stdout)
-    print("::error::Failed to install {' '.join(req)}.", flush=True)
+    print(pip_proc.stdout, end="")
+    print(f"::error::Failed to install {' '.join(req)}.", flush=True)
     sys.exit(pip_proc.returncode)
 
 
---
+++ action/test_main.py
@@ -0,0 +1,226 @@
+import re
+import sys
+from contextlib import contextmanager
+from pathlib import Path
+from runpy import run_module
+from subprocess import PIPE, STDOUT, CompletedProcess
+from types import SimpleNamespace
+from typing import Dict, Generator
+from unittest.mock import ANY, Mock, call, patch
+
+import pytest
+
+# pylint: disable=redefined-outer-name,unused-argument
+
+
+class SysExitCalled(Exception):
+    """Mock exception to catch a call to `sys.exit`"""
+
+
+@pytest.fixture
+def run_main_env() -> Dict[str, str]:
+    """By default, call `main.py` with just `GITHUB_ACTION_PATH` in the environment"""
+    return {}
+
+
+@contextmanager
+def patch_main(
+    tmp_path: Path,
+    run_main_env: Dict[str, str],
+    pip_returncode: int = 0,
+) -> Generator[SimpleNamespace, None, None]:
+    """Patch `subprocess.run`, `sys.exit` and environment variables
+
+    :param tmp_path: Path to use for the `GITHUB_ACTION_PATH` environment variable
+    :param run_main_env: [description]
+    :yield: An object with `.subprocess.run` and `.sys.exit` mock objects
+
+    """
+
+    def run(args, **kwargs):
+        returncode = pip_returncode if args[1:3] == ["-m", "pip"] else 0
+        return CompletedProcess(args, returncode, stdout="", stderr="")
+
+    run_mock = Mock(wraps=run)
+    exit_ = Mock(side_effect=SysExitCalled)
+    with patch("subprocess.run", run_mock), patch("sys.exit", exit_), patch.dict(
+        "os.environ", {"GITHUB_ACTION_PATH": str(tmp_path), **run_main_env}
+    ):
+
+        yield SimpleNamespace(
+            subprocess=SimpleNamespace(run=run_mock), sys=SimpleNamespace(exit=exit_)
+        )
+
+
+@pytest.fixture
+def main_patch(
+    tmp_path: Path, run_main_env: Dict[str, str]
+) -> Generator[SimpleNamespace, None, None]:
+    with patch_main(tmp_path, run_main_env) as run_main_fixture:
+        yield run_main_fixture
+
+
+def test_creates_virtualenv(tmp_path, main_patch):
+    """The GitHub action creates a virtualenv for Darker"""
+    with pytest.raises(SysExitCalled):
+
+        run_module("main")
+
+    assert main_patch.subprocess.run.call_args_list[0] == call(
+        [sys.executable, "-m", "venv", str(tmp_path / ".darker-env")],
+        check=True,
+    )
+
+
+@pytest.mark.kwparametrize(
+    dict(run_main_env={}, expect=["darker[isort]"]),
+    dict(run_main_env={"INPUT_VERSION": "1.5.0"}, expect=["darker[isort]==1.5.0"]),
+    dict(
+        run_main_env={"INPUT_LINT": "pylint"},
+        expect=["darker[isort]", "pylint"],
+    ),
+    dict(
+        run_main_env={"INPUT_LINT": "pylint,flake8"},
+        expect=["darker[isort]", "pylint", "flake8"],
+    ),
+    dict(
+        run_main_env={"INPUT_LINT": "  flake8  "},
+        expect=["darker[isort]", "flake8"],
+    ),
+    dict(
+        run_main_env={"INPUT_LINT": "  flake8  ,  pylint  "},
+        expect=["darker[isort]", "flake8", "pylint"],
+    ),
+)
+def test_installs_packages(tmp_path, main_patch, run_main_env, expect):
+    """Darker, isort and linters are installed in the virtualenv using pip"""
+    with pytest.raises(SysExitCalled):
+
+        run_module("main")
+
+    assert main_patch.subprocess.run.call_args_list[1] == call(
+        [
+            str(tmp_path / ".darker-env" / "bin" / "python"),
+            "-m",
+            "pip",
+            "install",
+        ]
+        + expect,
+        check=False,
+        stdout=PIPE,
+        stderr=STDOUT,
+        encoding="utf-8",
+    )
+
+
+@pytest.mark.parametrize(
+    "linters", ["foo", "  foo  ", "foo,bar", "  foo  ,  bar  ", "pylint,foo"]
+)
+def test_wont_install_unknown_packages(tmp_path, linters):
+    """Non-whitelisted linters raise an exception"""
+    with patch_main(tmp_path, {"INPUT_LINT": linters}) as main_patch, pytest.raises(
+        RuntimeError,
+        match=re.escape("'foo' is not supported as a linter by the GitHub Action"),
+    ):
+
+        run_module("main")
+
+    # only virtualenv `run` called, `pip` and `darker` not called
+    (venv_create,) = main_patch.subprocess.run.call_args_list
+    assert venv_create == call([ANY, "-m", "venv", ANY], check=True)
+    assert not main_patch.sys.exit.called
+
+
+@pytest.mark.kwparametrize(
+    dict(env={"INPUT_SRC": "."}, expect=["--revision", "HEAD^", "."]),
+    dict(
+        env={"INPUT_SRC": "subdir/ myfile.py"},
+        expect=["--revision", "HEAD^", "subdir/", "myfile.py"],
+    ),
+    dict(
+        env={"INPUT_SRC": ".", "INPUT_OPTIONS": "--isort"},
+        expect=["--isort", "--revision", "HEAD^", "."],
+    ),
+    dict(
+        env={"INPUT_SRC": ".", "INPUT_REVISION": "master..."},
+        expect=["--revision", "master...", "."],
+    ),
+    dict(
+        env={"INPUT_SRC": ".", "INPUT_COMMIT_RANGE": "master..."},
+        expect=["--revision", "master...", "."],
+    ),
+    dict(
+        env={
+            "INPUT_SRC": ".",
+            "INPUT_REVISION": "master...",
+            "INPUT_COMMIT_RANGE": "ignored",
+        },
+        expect=["--revision", "master...", "."],
+    ),
+    dict(
+        env={"INPUT_SRC": ".", "INPUT_LINT": "pylint,flake8"},
+        expect=["--lint", "pylint", "--lint", "flake8", "--revision", "HEAD^", "."],
+    ),
+    dict(
+        env={
+            "INPUT_SRC": "here.py there/too",
+            "INPUT_OPTIONS": "--isort --verbose",
+            "INPUT_REVISION": "master...",
+            "INPUT_COMMIT_RANGE": "ignored",
+            "INPUT_LINT": "pylint,flake8",
+        },
+        expect=[
+            "--isort",
+            "--verbose",
+            "--lint",
+            "pylint",
+            "--lint",
+            "flake8",
+            "--revision",
+            "master...",
+            "here.py",
+            "there/too",
+        ],
+    ),
+)
+def test_runs_darker(tmp_path, env, expect):
+    """Configuration translates correctly into a Darker command line"""
+    with patch_main(tmp_path, env) as main_patch, pytest.raises(SysExitCalled):
+
+        run_module("main")
+
+    darker = str(tmp_path / ".darker-env" / "bin" / "darker")
+    assert (
+        call([darker] + expect, check=False) in main_patch.subprocess.run.call_args_list
+    )
+
+
+def test_error_if_pip_fails(tmp_path, capsys):
+    """Returns an error and the pip error code if pip fails"""
+    with patch_main(tmp_path, {}, pip_returncode=42) as main_patch, pytest.raises(
+        SysExitCalled
+    ):
+
+        run_module("main")
+
+    assert main_patch.subprocess.run.call_args_list[-1] == call(
+        [ANY, "-m", "pip", "install", "darker[isort]"],
+        check=False,
+        stdout=PIPE,
+        stderr=STDOUT,
+        encoding="utf-8",
+    )
+    assert (
+        capsys.readouterr().out.splitlines()[-1]
+        == "::error::Failed to install darker[isort]."
+    )
+    main_patch.sys.exit.assert_called_once_with(42)
+
+
+def test_exits(main_patch):
+    """A successful run exits with a zero return code"""
+    with pytest.raises(SysExitCalled):
+
+        run_module("main")
+
+    main_patch.sys.exit.assert_called_once_with(0)
