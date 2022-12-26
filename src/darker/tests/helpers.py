440080bf316a09eb9f8c5f7d7f4dc145c079aecb
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-12-26 17:55:21 +0200

    Helper for faking `flynt` availability in tests

--- src/darker/tests/helpers.py
+++ src/darker/tests/helpers.py
@@ -4,7 +4,7 @@ import re
 import sys
 from contextlib import contextmanager, nullcontext
 from types import ModuleType
-from typing import Any, ContextManager, Dict, List, Optional, Union
+from typing import Any, ContextManager, Dict, Generator, List, Optional, Union
 from unittest.mock import patch
 
 import pytest
@@ -59,15 +59,36 @@ def raises_or_matches(expect, match_exc_attrs):
 
 
 @contextmanager
-def isort_present(present):
-    """Context manager to remove or add the `isort` package temporarily for a test"""
+def _package_present(
+    package_name: str, present: bool
+) -> Generator[Optional[ModuleType], None, None]:
+    """Context manager to remove or add a package temporarily for a test"""
     if present:
-        # Inject a dummy `isort` package temporarily
-        fake_isort_module: Optional[ModuleType] = ModuleType("isort")
-        # dummy function required by `import_sorting`:
-        fake_isort_module.code = None  # type: ignore
+        # Inject a dummy package temporarily
+        fake_module: Optional[ModuleType] = ModuleType(package_name)
     else:
         # Remove the `isort` package temporarily
-        fake_isort_module = None
-    with patch.dict(sys.modules, {"isort": fake_isort_module}):
+        fake_module = None
+    with patch.dict(sys.modules, {package_name: fake_module}):
+        yield fake_module
+
+
+@contextmanager
+def isort_present(present: bool) -> Generator[None, None, None]:
+    """Context manager to remove or add the `isort` package temporarily for a test"""
+    with _package_present("isort", present) as fake_isort_module:
+        if present:
+            # dummy function required by `import_sorting`:
+            fake_isort_module.code = None  # type: ignore
+        yield
+
+
+@contextmanager
+def flynt_present(present: bool) -> Generator[None, None, None]:
+    """Context manager to remove or add the `flynt` package temporarily for a test"""
+    with _package_present("flynt", present) as fake_flynt_module:
+        if present:
+            # dummy module and function required by `fstring`:
+            fake_flynt_module.process = ModuleType("process")  # type: ignore
+            fake_flynt_module.process.fstringify_code_by_line = None  # type: ignore
         yield
