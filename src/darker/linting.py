d361738b244321c4f9709cd610d764114886a68f
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-01-10 21:14:02 +0000

    Class to store linter message location in a file

--- src/darker/linting.py
+++ src/darker/linting.py
@@ -22,6 +22,7 @@ provided that the ``<linenum>`` falls on a changed line.
 import logging
 import shlex
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from subprocess import PIPE, Popen  # nosec
 from typing import IO, Generator, List, Set, Tuple
@@ -31,6 +32,33 @@ from darker.highlighting import colorize
 from darker.utils import WINDOWS
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass(eq=True, frozen=True, order=True)
+class MessageLocation:
+    """A file path, line number and column number for a linter message
+
+    Line and column numbers a 0-based, and zero is used for an unspecified column, and
+    for the non-specified location.
+
+    """
+
+    path: Path
+    line: int
+    column: int = 0
+
+    def __str__(self) -> str:
+        """Convert file path, line and column into a linter line prefix string
+
+        :return: Either ``"path:line:column"`` or ``"path:line"`` (if column is zero)
+
+        """
+        if self.column:
+            return f"{self.path}:{self.line}:{self.column}"
+        return f"{self.path}:{self.line}"
+
+
+NO_MESSAGE_LOCATION = MessageLocation(Path(""), 0, 0)
 
 
 def _strict_nonneg_int(text: str) -> int:
