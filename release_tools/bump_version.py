6cdd3744502b76160e0d057888766b4f0273c5ec
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-26 12:50:28 +0300

    Verbose string match error in `bump_version.py`

--- release_tools/bump_version.py
+++ release_tools/bump_version.py
@@ -419,7 +419,10 @@ def patch_changelog(next_version: Version, dry_run: bool) -> None:
     path = Path("CHANGES.rst")
     content = path.read_text(encoding="utf-8")
     before_unreleased = "These features will be included in the next release:\n\n"
-    insert_point = content.index(before_unreleased) + len(before_unreleased)
+    try:
+        insert_point = content.index(before_unreleased) + len(before_unreleased)
+    except ValueError as exc:
+        raise ValueError(f"{exc} {before_unreleased!r}") from exc
     before = content[:insert_point]
     after = content[insert_point:]
     title = f"{next_version}_ - {date.today()}"
