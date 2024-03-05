4e2c1ba034f373d04276a47352c3417e645083d7
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-03-05 22:18:27 +0200

    Fix test compatibility with Pygments 2.17.2

--- src/darker/tests/test_main.py
+++ src/darker/tests/test_main.py
@@ -911,6 +911,9 @@ def test_modify_file(tmp_path, new_content, expect):
             f'{BLUE}print{RESET}({YELLOW}"{RESET}foo{YELLOW}"{RESET})\n',
             # Pygments 2.14.0, variant 1:
             f'{CYAN}print{RESET}({YELLOW}"{RESET}foo{YELLOW}"{RESET}){WHITE}{RESET}\n',
+            # Pygments 2.17.2
+            f'{CYAN}print{RESET}({YELLOW}"{RESET}{YELLOW}foo{RESET}{YELLOW}"{RESET})'
+            f"{WHITE}{RESET}\n",
         ),
     ),
 )
