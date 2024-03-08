02c49bf06687674632f8943187d6a52d3f6f2f3f
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-03-08 17:18:52 +0200

    Turn `TestCase` into a Pytest style test function

--- src/darker/tests/test_files.py
+++ src/darker/tests/test_files.py
@@ -3,22 +3,20 @@
 import io
 from contextlib import redirect_stderr
 from pathlib import Path
-from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
 from darker import files
 
 
-class BlackTestCase(TestCase):
-    @patch("darker.files.find_user_pyproject_toml")
-    def test_find_pyproject_toml(self, find_user_pyproject_toml: MagicMock) -> None:
-        find_user_pyproject_toml.side_effect = RuntimeError()
+@patch("darker.files.find_user_pyproject_toml")
+def test_find_pyproject_toml(find_user_pyproject_toml: MagicMock) -> None:
+    """Test `files.find_pyproject_toml` with no user home directory."""
+    find_user_pyproject_toml.side_effect = RuntimeError()
+    with redirect_stderr(io.StringIO()) as stderr:
+        # end of test setup
 
-        with redirect_stderr(io.StringIO()) as stderr:
-            result = files.find_pyproject_toml(
-                path_search_start=(str(Path.cwd().root),)
-            )
+        result = files.find_pyproject_toml(path_search_start=(str(Path.cwd().root),))
 
-        assert result is None
-        err = stderr.getvalue()
-        assert "Ignoring user configuration" in err
+    assert result is None
+    err = stderr.getvalue()
+    assert "Ignoring user configuration" in err
