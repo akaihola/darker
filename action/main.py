9e828aa1cede0ffe454e34b9b377819ce0d6b071
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-08-01 23:08:14 +0300

    feat(action,lint): ignore `lint:` input option

--- action/main.py
+++ action/main.py
@@ -6,16 +6,12 @@ import sys
 from pathlib import Path
 from subprocess import PIPE, STDOUT, run  # nosec
 
-from pip_requirements_parser import parse_reqparts_from_string
-
-LINTER_WHITELIST = {"flake8", "pylint", "mypy"}
 ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
 ENV_PATH = ACTION_PATH / ".darker-env"
 ENV_BIN = ENV_PATH / ("Scripts" if sys.platform == "win32" else "bin")
 OPTIONS = os.getenv("INPUT_OPTIONS", default="")
 SRC = os.getenv("INPUT_SRC", default="")
 VERSION = os.getenv("INPUT_VERSION", default="")
-LINT = os.getenv("INPUT_LINT", default="")
 REVISION = os.getenv("INPUT_REVISION") or os.getenv("INPUT_COMMIT_RANGE") or "HEAD^"
 
 if os.getenv("INPUT_LINT", default=""):
@@ -34,18 +30,6 @@ if VERSION:
         req[0] += VERSION
     else:
         req[0] += f"=={VERSION}"
-linter_options = []
-for requirement_string in LINT.split(","):
-    if not requirement_string.strip():
-        continue
-    linter_requirement = parse_reqparts_from_string(requirement_string).requirement
-    linter = linter_requirement.name
-    if linter not in LINTER_WHITELIST:
-        raise RuntimeError(
-            f"{linter!r} is not supported as a linter by the GitHub Action"
-        )
-    req.append(str(linter_requirement))
-    linter_options.extend(["--lint", linter])
 
 pip_proc = run(  # nosec
     [str(ENV_BIN / "python"), "-m", "pip", "install"] + req,
@@ -65,7 +49,6 @@ proc = run(  # nosec
     [
         *base_cmd,
         *shlex.split(OPTIONS),
-        *linter_options,
         "--revision",
         REVISION,
         *shlex.split(SRC),
