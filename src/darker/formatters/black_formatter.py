556277442fce0a2c8fb479751d989eb6f97ad6bc
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-10-06 19:56:52 +0300

    feat: add formatter TOML config sections to make testing easier

--- src/darker/formatters/black_formatter.py
+++ src/darker/formatters/black_formatter.py
@@ -83,6 +83,8 @@ class BlackFormatter(BaseFormatter, HasConfig[BlackCompatibleConfig]):
 
     config: BlackCompatibleConfig  # type: ignore[assignment]
 
+    config_section = "tool.black"
+
     def read_config(self, src: tuple[str, ...], args: Namespace) -> None:
         """Read Black configuration from ``pyproject.toml``.
 
