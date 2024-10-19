c45f9345471390994f355ebf4cb33c6de86817bc
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-10-19 22:13:44 +0200

    fix: only log flynt result if --flynt enabled

--- src/darker/__main__.py
+++ src/darker/__main__.py
@@ -209,11 +209,6 @@ def _reformat_and_flynt_single_file(  # noqa: PLR0913
         relative_path_in_rev2, exclude.flynt, edited_linenums_differ, rev2_isorted
     )
     has_fstring_changes = fstringified != rev2_isorted
-    logger.debug(
-        "Flynt resulted in %s lines, with %s changes from fstringification",
-        len(fstringified.lines),
-        "some" if has_fstring_changes else "no",
-    )
     # 3. run the code re-formatter on the isorted and fstringified contents of each
     #    edited to-file
     formatted = _maybe_reformat_single_file(
@@ -279,7 +274,13 @@ def _maybe_flynt_single_file(
     if glob_any(relpath_in_rev2, exclude):
         # `--flynt` option not specified, don't reformat
         return rev2_isorted
-    return apply_flynt(rev2_isorted, relpath_in_rev2, edited_linenums_differ)
+    result = apply_flynt(rev2_isorted, relpath_in_rev2, edited_linenums_differ)
+    logger.debug(
+        "Flynt resulted in %s lines, with %s changes from fstringification",
+        len(result.lines),
+        "some" if result != rev2_isorted else "no",
+    )
+    return result
 
 
 def _maybe_reformat_single_file(
